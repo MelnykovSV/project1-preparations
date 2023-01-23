9f4ad8f695715c6fe1d37d96861e3eea3aa265ab
MelnykovSV <svmelnykov@gmail.com>
2023-01-24 01:38:45 +0200

    arrows

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,9 +1,9 @@
 $('.responsive').slick({
   dots: true,
   prevArrow:
-    '<button type="button" class="slick-header-prev"><svg class="slick-header-prev-icon"class="slick-header-prev" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M31 15.49h-26.16l6.019-6.061-1.419-1.429-8.44 8.5 8.44 8.5 1.419-1.429-6.019-6.061h26.16v-2.021z"></path></svg></button>',
+    '<button type="button" class="slick-header-prev"><svg class="slick-header-prev-icon" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M31 15.49h-26.16l6.019-6.061-1.419-1.429-8.44 8.5 8.44 8.5 1.419-1.429-6.019-6.061h26.16v-2.021z"></path></svg></button>',
   nextArrow:
-    '<button type="button" class="slick-header-next"><svg class="slick-header-next-icon" width="40" height="40"><use href="images/icons.svg#icon-arrow-right"></use></svg></button>',
+    '<button type="button" class="slick-header-next"><svg class="slick-header-next-icon" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M22.559 8l-1.419 1.429 6.019 6.061h-26.16v2.021h26.159l-6.018 6.061 1.419 1.429 8.441-8.5-8.441-8.5z"></path></svg></button>',
   infinite: true,
   speed: 300,
   slidesToScroll: 1,
