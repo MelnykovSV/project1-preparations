c6311f823bde14143a4387f854b8a53a3027894f
MelnykovSV <svmelnykov@gmail.com>
2023-01-24 02:27:40 +0200

    modal

--- src/js/experimental-slick.js
+++
@@ -1,7 +0,0 @@
-$('.multiple-items').slick({
-  prevArrow: '<button type="button" class="my-slick-prev">&xlarr;</button>',
-  nextArrow: '<button type="button" class="my-slick-next">&xrarr;</button>',
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-});
